edfc157aad3c4aad58b9b38eeea67e643eddcb96
Sambid10 <sambidshk10@gmail.com>
2025-06-17 08:52:51 +0000

    fk problem

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
   validates:photo,:description,presence:true
-  has_many:comments
+  has_many:comments,dependent: :delete_all
 
   acts_as_votable
 end
